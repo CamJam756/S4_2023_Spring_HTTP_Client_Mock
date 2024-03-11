f120011b27149047599bf92f503a832497c6ce1d
Cameron Penney <127319515+CamJam756@users.noreply.github.com>
2024-03-11 12:24:43 -0230

    Add files via upload

---
+++ S4_2023_Spring_HTTP_Client_Mock/src/main/java/com/keyin/http/client/RESTClient.java
@@ -0,0 +1,57 @@
+package com.keyin.http.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keyin.domain.Airport;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RESTClient {
+    private String baseUrl;
+
+    public RESTClient(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public List<Airport> getAllAirports() {
+        List<Airport> airports = new ArrayList<Airport>();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseUrl + "/airports")).build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("***** " + response.body());
+            } else {
+                System.out.println("Error Status Code: " + response.statusCode());
+            }
+
+            airports = buildAirportListFromResponse(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return airports;
+    }
+
+    public List<Airport> buildAirportListFromResponse(String response) throws JsonProcessingException {
+        List<Airport> airports = new ArrayList<Airport>();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        airports = mapper.readValue(response, new TypeReference<List<Airport>>() {
+        });
+
+        return airports;
+    }
+}
